3e9f87d19435152601780f74cc92b36baa08bb9e
martinvplopez <martinvplopez@gmail.com>
2021-08-20 11:43:51 +0200

    Roman converter

--- smallprojects/JS challenges/test.js
+++ smallprojects/JS challenges/test.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 //      Martín van Puffelen López 
 //      2021
 //      Challenge done for FreeCodeCamp´s DS and Algorithms: Roman 
 
-=======
->>>>>>> 29b4fe6520319748d0927f51154599ae6c786e0a
 const romanFigures={ // Representation of numbers in roman.
     1:"I",
     5: "V",
@@ -15,7 +12,6 @@ const romanFigures={ // Representation of numbers in roman.
     1000:"M"
 }
 
-<<<<<<< HEAD
 // This main function will get a decimal number as an input and will return a string with its roman representation.
 function romanConverter(num){
     const figures= getFigure(num);
@@ -63,53 +59,6 @@ function romanConverter(num){
                     figures[i]-= auxKey;
                     romanRes+=romanFigures[auxKey];
                     if(pos!=0 && figures[i]-auxKey<0){ // Only get a lower value from the roman object if it is possible (not 0) and needed (the substraction returns a negative number)
-=======
-//console.log(romanFigures[1]);
-
-function romanConversor(num){
-    const figures= getCipher(num);
-    let romanRes="";
-    for (let i = 0; i < figures.length; i++) {
-        for (let j = 0; j < 7; j++) { // 7 is the length of the object
-            let auxKey= Object.keys(romanFigures)[j] 
-            if(figures[i]==auxKey){
-                romanRes+= romanFigures[auxKey];
-                break;
-            }else if(figures[i]<auxKey){
-                //console.log(romanRes);
-                if(figures[i]===auxKey-1){ //9==10-1
-                    console.log("Entra en el -1");
-                    let pos;
-                    if(pos-2>0){
-                        pos=j-2;
-                    }else{
-                        pos=j-1;
-                    }
-                    let auxKeyPrev=  Object.keys(romanFigures)[pos]; 
-                    romanRes+= romanFigures[auxKeyPrev] + romanFigures[auxKey]; 
-                    break;
-                }
-                if(figures[i]===(auxKey-10)){
-                    console.log(figures[0]);
-                    console.log(auxKey);
-                    console.log("Entra en el -10");
-                    let auxKeyX=  Object.keys(romanFigures)[2]; 
-                    romanRes+= romanFigures[auxKeyX] + romanFigures[auxKey]; 
-                    break;
-                }
-                let auxKeyPrev=  Object.keys(romanFigures)[j-1]; 
-                if(figures[i]===auxKey-auxKeyPrev){
-                    console.log("Entra en el -ant");
-                    romanRes += romanFigures[auxKeyPrev] + romanFigures[auxKey];
-                    break;
-                }
-                let pos=j-1;
-                auxKey=  Object.keys(romanFigures)[pos]; 
-                while(figures[i]-auxKey>=0){
-                    figures[i]-= auxKey;
-                    romanRes+=romanFigures[auxKey];
-                    if(pos!=0 && figures[i]-auxKey<0){
->>>>>>> 29b4fe6520319748d0927f51154599ae6c786e0a
                         pos--;
                         auxKey= Object.keys(romanFigures)[pos]; 
                     }
@@ -121,7 +70,6 @@ function romanConversor(num){
     return romanRes;
 }
 
-<<<<<<< HEAD
 
 
 // Function to get the integer figures of a number
@@ -130,37 +78,16 @@ function getFigure(num) {
     let figures=[];
     const numS= num.toString();
     if(numS.length===1){ // Trivial case, only one figure
-=======
-romanConversor(3);
-
-
-
-
-//Function to get the integer ciphers of a number
-//e.g: 286 is 200 + 80 + 6
-function getCipher(num) {
-    let figures=[];
-    const numS= num.toString();
-    if(numS.length===1){
->>>>>>> 29b4fe6520319748d0927f51154599ae6c786e0a
         figures.push(num);
     }else{
         let g=0;
         while(g<numS.length){
             let figure=numS[g];
-<<<<<<< HEAD
             if(figure==="0"){ // If the digit is a 0 just continue through the number
                 g++;
                 figure=numS[g];
             }else{ // Add as many 0´s as digits are left in the number
                 for(let i=g;i<numS.length-1;i++){ 
-=======
-            if(figure==="0"){
-                g++;
-                figure=numS[g];
-            }else{
-                for(let i=g;i<numS.length-1;i++){
->>>>>>> 29b4fe6520319748d0927f51154599ae6c786e0a
                     figure += "0";
                 }
                 figures.push(parseInt(figure));
@@ -171,7 +98,6 @@ function getCipher(num) {
     console.log(figures);
     return figures;
 }
-<<<<<<< HEAD
 
 // Some tests of the functionality to check if it works :)
 
@@ -189,5 +115,3 @@ if ("M"===romanConverter(1000)) {
     console.log(true);
 }
 
-=======
->>>>>>> 29b4fe6520319748d0927f51154599ae6c786e0a
