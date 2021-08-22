bd1e6de1ce1c7206f171f847d0fa3e68d46423c3
martinvplopez <vanpu3d@gmail.com>
2021-08-22 12:08:58 +0200

    Commit of caesar cipher decription

--- smallprojects/JS challenges/caesar.js
+++ smallprojects/JS challenges/caesar.js
@@ -7,14 +7,20 @@
 function rot13(str){
     let strRes="";
     for (let index = 0; index < str.length; index++) {
-        if (/\w/.test(str[index])) { // Checking if it is an alphabetic number
-            let strVal= str.charCodeAt(index) - 13;
+        if (/\w/.test(str[index])) { // Checking if it is an alphabetic number.
+            let strVal= str.charCodeAt(index) - 13; // Decoding is just making the opposit shift.
+            if (strVal<65) { // The values have to be in the 65-90 range in ASCII code.
+                strVal+=26;
+            }
             strRes+= String.fromCharCode(strVal);
+        }else{ // Values which aren´t 
+            strRes+= str[index];
         }
     }
     return strRes;
 }
 
-// Algorithm: go through the string checking every char and if it is an alphabetic character substract it 13.
-
-console.log(rot13("N"));
+// Functionality checks.
+console.log(rot13("SERR YBIR?")); // Has to return FREE LOVE?
+console.log(rot13("SERR CVMMN!")); // Has to return FREE PIZZA!
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); // THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
