251585c17db37fbf6c899c80093c4e9bf6ef3955
martinvplopez <vanpu3d@gmail.com>
2021-08-24 18:41:45 +0200

    Telephone checker finished with regex

--- smallprojects/JS challenges/telephone.js
+++ smallprojects/JS challenges/telephone.js
@@ -2,21 +2,27 @@
 //      2021
 //      Challenge done for FreeCodeCamp´s DS and Algorithms: Telephone Number Validator
 
-// The funtion gets a string as input and has to determine whether it´s a valid US telephone number or not.
-function telephoneCheck(str) {
-    if (str.length>16 || str.length<10) {
-        return false;
-    }
-    let regex= /\d |\s |\( |\)/; // It will search for digits, spaces and parenthesis.
-    if (regex.test(str)) {
-        console.log("Lleva digito, espacio o parentesis");
-        return false;
-    }
-    
-    
-    
-    //return true;
+//      https://regex101.com/ is a very useful page to check in realtime for patterns in strings.
 
+// The function gets a string as input and has to determine whether it´s a valid US telephone number or not.
+function telephoneCheck(str) {
+    let regex=/^(1\s?)?(\d{3}|\(\d{3}\))(\s|\-)?\d{3}(\s|\-)?(\d{4})$/;
+    return regex.test(str);
 }
 
-telephoneCheck("999-(");
+
+
+// Regex explanation
+// The requirements for a valid telephone are set by FreeCodeCamp so the regex will check for:
+//  - The country code 1 as first character, not required. 
+//  - Then it will match the 3 digits for the area code, it can be in parenthesis. It will be separated with other 3 digits with a space or a "-". Same happens with the last four digits.   
+// Some valid numbers:
+//     555-555-5555
+//     (555)555-5555
+//     (555) 555-5555
+//     555 555 5555
+//     5555555555
+//     1 555 555 5555
+
+
+
